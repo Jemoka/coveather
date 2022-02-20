a419a9aa217250c2e6876d4c11f66e048166154d
Houjun Liu <hliu@shabang.cf>
2022-02-19 16:45:57 -0800

    please its not the M1 is it?

--- src/coveather/core.clj
+++ src/coveather/core.clj
@@ -1,7 +1,12 @@
 (ns coveather.core
+  (:import (org.zeromq ZMQ))
   (:gen-class))
+
+
+(ZMQ/context 0)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
